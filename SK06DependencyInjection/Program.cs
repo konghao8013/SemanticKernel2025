35932b9c6426758f768842756d4a3ec2d321418e
konghao <konghao8013@163.com>
2025-06-23 21:51:37 +0800

    AddKeyedTransient 按键注册服务

---
+++ SK06DependencyInjection/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        // 初始化依赖注入容器
+        var services = new ServiceCollection();
+
+        // 注册Kernel服务
+        services.RegisterKernels();
+
+        // 构建ServiceProvider
+        var serviceProvider = services.BuildServiceProvider();
+
+
+        // 获取Kernel服务
+        var kernel = serviceProvider.GetKeyedService<Kernel>("zhipu");
+        var response = await kernel.InvokePromptAsync("一句话简单介绍Semantic Kernel。");
+        Console.WriteLine(response.GetValue<string>());
+
+        var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+        var chatCompletionResponse = await chatCompletionService.GetChatMessageContentAsync("一句话简单介绍LangChain。");
+        Console.WriteLine(chatCompletionResponse.Content);
+    }
+
+}
+
---
+++ SK06DependencyInjection/ServiceExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection RegisterKernels(this IServiceCollection services)
+    {
+        // 从配置文件中加载AI配置
+        var aiOptions = AiSettings.LoadAiProvidersFromFile();
+        // 注册其他AI服务提供商
+        foreach (var aiProvider in aiOptions.Providers)
+        {
+            var providerRegister = AiProviderRegisterFactory.Create(aiProvider!.AiType);
+
+            providerRegister.Register(services, aiProvider);
+        }
+        return services;
+    }
+    public static IServiceCollection AddNLogging(this IServiceCollection services)
+    {
+        // 定义文件日志输出目标
+        var fileTarget = new FileTarget()
+        {
+            FileName = "sk-demo.log",
+            AutoFlush = true,
+            DeleteOldFileOnStartup = true
+        };
+        // 定义控制台日志输出目标
+        var consoleTarget = new ConsoleTarget();
+        var config = new NLog.Config.LoggingConfiguration();
+        // 定义日志输出规则(输出所有Trace级别及以上的日志到控制台)
+        config.AddRule(
+            NLog.LogLevel.Trace,
+            NLog.LogLevel.Fatal,
+            target: fileTarget,  // 这里采用文件输出
+            "*");// * 表示所有Logger
+                 // 注册NLog
+        services.AddLogging(loggingBuilder => loggingBuilder.AddNLog(config));
+        return services;
+    }
+}
+
