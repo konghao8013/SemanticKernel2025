15d2756b9eff7acba119c9d82545c06529865b88
konghao <konghao8013@163.com>
2025-06-24 22:51:52 +0800

    安装 Microsoft.SemanticKernel.PromptTemplates.Liquid

--- SK12LinquidTemplate/Program.cs
+++ SK12LinquidTemplate/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+Console.WriteLine("请输入AI服务提供商编码：");
+var aiProviderCode = Console.ReadLine();
+
+var kernel = ServiceExtensions.GetKernel(aiProviderCode);
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+
+var executionSettings = new OpenAIPromptExecutionSettings()
+{
+    Temperature = 0.1f,
+    MaxTokens = 50,
+    TopP = 0.9f
+};
+
+
+
+async Task Chat(string userPrompt)
+{
+    var response = await chatCompletionService.GetChatMessageContentAsync(userPrompt, executionSettings);
+    Console.WriteLine($"AI Response: {response.Content}");
+}
+
+
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
