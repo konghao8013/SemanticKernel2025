18494998b103956cbed720f6ce3ead1cbe88c0eb
konghao <konghao8013@163.com>
2025-06-24 22:10:58 +0800

    1.增加自定义函数获取天气温度

---
+++ SK10PromptTemplate/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Services;
+
+Console.WriteLine("请输入AI服务提供商编码：");
+var aiProviderCode = Console.ReadLine();
+
+var kernel = ServiceExtensions.GetKernel(aiProviderCode);
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+
+var executionSettings = new OpenAIPromptExecutionSettings()
+{
+    Temperature = 0.1f,
+    MaxTokens = 50,
+    TopP = 0.9f
+};
+
+
+
+async Task Chat(string userPrompt)
+{
+    var response = await chatCompletionService.GetChatMessageContentAsync(userPrompt, executionSettings);
+    Console.WriteLine($"AI Response: {response.Content}");
+}
+
+
+
+var prompt = "The weather today in {{ $location }} is {{GetForecast}}.";
+
+kernel.Plugins.Clear();
+kernel.Plugins.AddFromType<WeatherPlugin>();
+
+var res = await kernel.InvokePromptAsync(prompt, new() { ["location"] = "Shenzhen" });
+Console.WriteLine($"AI Response: {res.GetValue<String>()}");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ SK10PromptTemplate/WeatherPlugin.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class WeatherPlugin
+{
+    [KernelFunction("GetForecast")]
+    [Description("获取给定位置的天气预报")]
+    [return: Description("指定位置的天气预报")]
+    public static string GetForecast(string location)
+    {
+        return $"Sunny, 23℃";
+    }
+}
