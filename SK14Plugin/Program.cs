3ad9faa483b3c03ad567141a9ef3ec1522919643
konghao <konghao8013@163.com>
2025-06-26 22:13:18 +0800

    函数的调用手动调用，自动调用

--- SK14Plugin/Program.cs
+++ SK14Plugin/Program.cs
@@ -1,31 +1,80 @@
 ﻿
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 using SK14Plugin.Plugins;
+using System.Text.Json;
 
-var builder = Kernel.CreateBuilder();
-var kernel = builder.Build();
+//var builder = Kernel.CreateBuilder();
+//var kernel = builder.Build();
 
+//kernel.Plugins.AddFromFunctions("time_plugin",
+//[
+//   KernelFunctionFactory.CreateFromMethod(
+//        method: () => DateTime.Now,
+//        functionName: "get_time",op
+//        description: "Get the current time"
+//    ),
+//    KernelFunctionFactory.CreateFromMethod(
+//        method: (DateTime start, DateTime end) => (end - start).TotalSeconds,
+//        functionName: "diff_time",
+//        description: "Get the difference between two times in seconds"
+//    )
+//]);
+
+//var time = await kernel.InvokeAsync<DateTime>("time_plugin", "get_time", new());
+//Console.WriteLine($"The current time is {time}.");
+
+//kernel.Plugins.Clear();
+//kernel.Plugins.AddFromType<MathPlugin>();
+//// Test the math plugin
+//double answer = await kernel.InvokeAsync<double>("MathPlugin", "Sqrt", new() { { "number1", 9 } });
+//Console.WriteLine($"The square root of 9 is {answer}.");
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+// 引入交互式的内核命名空间，以便用户输入
+Console.WriteLine("请输入AI服务提供商编码：");
+var aiProviderCode = Console.ReadLine();
+
+var kernel = ServiceExtensions.GetKernel(aiProviderCode);
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+
+
+// 注册插件
+kernel.Plugins.Clear();
+kernel.Plugins.AddFromType<MathPlugin>();
 kernel.Plugins.AddFromFunctions("time_plugin",
 [
    KernelFunctionFactory.CreateFromMethod(
-        method: () => DateTime.Now,
-        functionName: "get_time",
-        description: "Get the current time"
-    ),
-    KernelFunctionFactory.CreateFromMethod(
-        method: (DateTime start, DateTime end) => (end - start).TotalSeconds,
-        functionName: "diff_time",
-        description: "Get the difference between two times in seconds"
-    )
+       method: () => {
+       return $"{DateTime.Now.AddHours(-8).ToString("yyyy年MM月dd日 HHmmss.fff")}";
+       },
+       functionName: "get_time",
+       description: "Get the current time"
+   )
 ]);
 
-var time = await kernel.InvokeAsync<DateTime>("time_plugin", "get_time", new());
-Console.WriteLine($"The current time is {time}.");
 
-kernel.Plugins.Clear();
-kernel.Plugins.AddFromType<MathPlugin>();
-// Test the math plugin
-double answer = await kernel.InvokeAsync<double>("MathPlugin", "Sqrt", new() { { "number1", 9 } });
-Console.WriteLine($"The square root of 9 is {answer}.");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("请输入你的请求：");
+var request=Console.ReadLine();
+// Enable auto function calling
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+// Get the response from the AI
+var result = await chatCompletionService.GetChatMessageContentAsync(
+    prompt: request,
+    executionSettings: openAIPromptExecutionSettings,
+    kernel: kernel);
+
+
+
+
+Console.WriteLine(JsonSerializer.Serialize(result));
+Console.WriteLine($"AI Response: {result.Content}");
+Console.WriteLine("How word!");
---
+++ SK15FunctionCallingBehavior/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+Console.WriteLine("请输入AI服务提供商编码：");
+var aiProviderCode = Console.ReadLine();
+
+var kernel = ServiceExtensions.GetKernel(aiProviderCode);
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+
+kernel.Plugins.Clear();
+kernel.ImportPluginFromFunctions("HelperFunctions",
+        [
+            kernel.CreateFunctionFromMethod(() => DateTime.UtcNow.ToString("R"), "GetCurrentDateTimeInUtc", "Retrieves the current date time in UTC."),
+            kernel.CreateFunctionFromMethod((string cityName, string currentDateTimeInUtc) =>
+                cityName switch
+                {
+                    "Boston" => "61 and rainy",
+                    "London" => "55 and cloudy",
+                    "Miami" => "80 and sunny",
+                    "Paris" => "60 and rainy",
+                    "Tokyo" => "50 and sunny",
+                    "Sydney" => "75 and sunny",
+                    "Tel Aviv" => "80 and sunny",
+                    _ => "31 and snowing",
+                }, "GetWeatherForCity", "Gets the current weather for the specified city and specified date time."),
+        ]);
+
+// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Hello, World!");
