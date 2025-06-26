3c5dd7fd553c9cc0820ff03ff0739169f07b69a3
konghao <konghao8013@163.com>
2025-06-26 23:29:28 +0800

    写不同的说明，注意Semantic Kernel是对API调用的封装，注意查看调用日志

--- SK18FunctionCallingReturn/Program.cs
+++ SK18FunctionCallingReturn/Program.cs
@@ -13,11 +13,27 @@ var kernel = ServiceExtensions.GetKernel(aiProviderCode);
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
 
-kernel.Plugins.Clear();
-kernel.ImportPluginFromType<WeatherPlugin1>();
+//kernel.Plugins.Clear();
+//kernel.ImportPluginFromType<WeatherPlugin1>();
 
+//OpenAIPromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+//FunctionResult result = await kernel.InvokePromptAsync("What is the current weather?", new(settings));
+//Console.WriteLine(result);
+
+//kernel.Plugins.Clear();
+//kernel.ImportPluginFromType<WeatherPlugin2>();
+
+//OpenAIPromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+//FunctionResult result = await kernel.InvokePromptAsync("What is the current weather?", new(settings));
+//Console.WriteLine(result);
+
+
+kernel.AutoFunctionInvocationFilters.Add(new AddReturnTypeSchemaFilter());
+// Import the plugin that provides descriptions for the return type properties.   
+// This additional information is used when extracting the schema from the return type.
+kernel.Plugins.Clear();
+kernel.ImportPluginFromType<WeatherPlugin3>();
 OpenAIPromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
 FunctionResult result = await kernel.InvokePromptAsync("What is the current weather?", new(settings));
 Console.WriteLine(result);
-
 Console.WriteLine("Hello, World!");
