8b279ca145a2e52543fa9e9f6c582b95ad69bf7a
konghao <konghao8013@163.com>
2025-06-27 22:07:35 +0800

    init 初始化

--- SK22Planner/Program.cs
+++ SK22Planner/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// 1. 配置自动函数调用
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+Console.WriteLine("请输入AI服务提供商编码：");
+var aiProviderCode = Console.ReadLine();
+
+var kernel = ServiceExtensions.GetKernel(aiProviderCode);
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+OpenAIPromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+// 2. 注册插件（自动暴露函数描述）
+kernel.ImportPluginFromFunctions("FinancePlugin", [
+    kernel.CreateFunctionFromMethod(()=>{
+    return "20人名币";
+    }, "GetStockPrice", "获取股票实时价格")
+]);
+
+// 3. 直接对话触发
+var chatResult = await chatCompletionService.GetChatMessageContentAsync(
+    "查询微软股票的最新价格", settings, kernel
+);
+Console.WriteLine(chatResult.Content);
+
 Console.WriteLine("Hello, World!");
---
+++ SK23StepwisePlanner/Plugins/MathPlugin.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK14Plugin.Plugins
+{
+    public class MathPlugin
+    {
+        [KernelFunction, Description("Take the square root of a number")]
+        public static double Sqrt(
+            [Description("The number to take a square root of")] double number1
+        )
+        {
+            Console.WriteLine("Taking the square root of {0}", number1);
+            return Math.Sqrt(number1);
+        }
+        [KernelFunction, Description("Add two numbers")]
+        public static double Add(
+            [Description("The first number to add")] double number1,
+            [Description("The second number to add")] double number2
+        )
+        {
+            Console.WriteLine("Adding {0} to {1}", number1, number2);
+            return number1 + number2;
+        }
+        [KernelFunction, Description("Subtract two numbers")]
+        public static double Subtract(
+            [Description("The first number to subtract from")] double number1,
+            [Description("The second number to subtract away")] double number2)
+        {
+            Console.WriteLine("Subtracting {0} from {1}", number2, number1);
+            return number1 - number2;
+        }
+        [KernelFunction, Description("Multiply two numbers. When increasing by a percentage, don't forget to add 1 to the percentage.")]
+        public static double Multiply(
+            [Description("The first number to multiply")] double number1,
+            [Description("The second number to multiply")] double number2
+        )
+        {
+            Console.WriteLine("Multiplying {0} by {1}", number1, number2);
+            return number1 * number2;
+        }
+        [KernelFunction, Description("Divide two numbers")]
+        public static double Divide(
+            [Description("The first number to divide from")] double number1,
+            [Description("The second number to divide by")] double number2
+        )
+        {
+            Console.WriteLine("Dividing {0} by {1}", number1, number2);
+            return number1 / number2;
+        }
+    }
+}
